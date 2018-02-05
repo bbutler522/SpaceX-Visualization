c2271093dbf18e6a999973509051dfe229c8159f
Brennan Butler <bbutler522@gmail.com>
2018-02-05 12:55:47 -0500

    Add next launch generation and countdown

--- main.js
+++ main.js
@@ -1,6 +1,128 @@
 // https://api.spacexdata.com/v2/launches
 // https://api.spacexdata.com/v2/launches/upcoming
 
+// Countdown til launch
+// Set the date we're counting down to
+function launchCountdown(launchTime){
+  var countDownDate = new Date(1517941800 * 1000);
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("launchCountdown").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("launchCountdown").innerHTML = "";
+    }
+    $('#launchCountdown').css('opacity','1')
+  }, 1000);
+}
+// End Countdown Function
+
+
+// Generate next launch
+function nextLaunchGen(json) {
+  var html = "";
+  var val = json[0];
+  var num = val.flight_number;
+  var launchDate = new Date(val.launch_date_unix * 1000);
+  html += "<div data-launch='" + num + "' class='launch card'>";
+  html += "<div class='card-header toggle-header'>";
+  html += "<p><strong>Flight #:</strong> " + val.flight_number + "</p>";
+  html += "</div>";
+  html += "<div class='card-block'>";
+  html +=
+    "<p class='date'><strong>Launch Date:</strong> " +
+    launchDate.toLocaleDateString() +
+    "</p>";
+  html +=
+    "<p><strong>Rocket Name:</strong> " + val.rocket.rocket_name + "</p>";
+  html +=
+    "<p><strong>Rocket Type:</strong> " + val.rocket.rocket_type + "</p>";
+  var reuse = val.reuse;
+  $.each(reuse, function(key, value) {
+    if (value === true) {
+      html += '<p class="text-info"><strong>Reused ' + key + "</strong></p>";
+    }
+  });
+
+  /* Accordion for Details and Payload */
+  html +=
+    '<div class="accordion" id="accordion' +
+    num +
+    '" role="tablist" aria-multiselectable="true">';
+  if (val.details !== null) {
+    html +=
+      '<div class="card border-left-0 border-right-0  border-left-0 border-bottom-0"><div class="card-header" role="tab" id="headingOne' +
+      num +
+      '"><h5 class="mb-0"><a data-toggle="collapse" data-parent="#accordion' +
+      num +
+      '" href="#collapseOne' +
+      num +
+      '" aria-expanded="true" aria-controls="collapseOne' +
+      num +
+      '">Details</a></h5></div><div id="collapseOne' +
+      num +
+      '" class="collapse " role="tabpanel" aria-labelledby="headingOne' +
+      num +
+      '"><div class="card-block">' +
+      val.details +
+      "</div>";
+  }
+
+  html +=
+    '</div></div><div class="card  border-left-0 border-right-0 border-bottom-0"><div class="card-header" role="tab" id="headingThree' +
+    num +
+    '"><h5 class="mb-0"><a class="collapsed" data-toggle="collapse" data-parent="#accordion' +
+    num +
+    '" href="#collapseThree' +
+    num +
+    '" aria-expanded="false" aria-controls="collapseThree' +
+    num +
+    '">Payload</a></h5></div><div id="collapseThree' +
+    num +
+    '" class="collapse" role="tabpanel" aria-labelledby="headingThree' +
+    num +
+    '"><div class="card-block">';
+  // For each payload do
+  var payload = val.rocket.second_stage.payloads;
+  for (i = 0; i < payload.length; i++) {
+    html += "<p><strong>ID:</strong> " + payload[i].payload_id + "</p>";
+    html += "<p><strong>Type:</strong> " + payload[i].payload_type + "</p>";
+
+    html += "<p><strong>Customers:</strong> ";
+    // Get the customers
+    for (j = 0; j < payload[i].customers.length; j++) {
+      html += "<br/>" + payload[i].customers[j];
+    }
+    html += "<hr/>";
+  }
+
+  html += "</div></div></div>";
+  html += "</div>";
+
+  html += "</div>";
+  html += "</div>";
+  return html;
+
+};
+
 // Function to generate launches from JSON input
 function launchGen(json) {
   var html = "";
@@ -189,7 +311,24 @@ $.getJSON("https://api.spacexdata.com/v2/launches/upcoming", function(json) {
   // Add the html to the page
   $("#upcoming").append(html);
 });
+// End Upcoming Launches
 
+// Next Launch
+$.getJSON("https://api.spacexdata.com/v2/launches/upcoming", function(json) {
+  var html = "";
+
+  // Create the html for each flight
+  html = nextLaunchGen(json);
+
+  // Add the html to the page
+  $("#nextLaunch").append(html);
+
+  // Add countdown until launch
+  launchCountdown(json[0].launch_date_unix);
+});
+// End Next Launch
+
+// Sorting Previous Launches
 $("#sortLaunchFirst").on("click", sortLaunchNumFirst);
 $("#sortLaunchLast").on("click", sortLaunchNumLast);
 
@@ -212,3 +351,11 @@ function sortLaunchNumLast() {
   $("#sortLaunchLast").hide();
   $("#sortLaunchFirst").show();
 }
+// End Sorting Previous Launches
+
+// Show additional next launch info
+$("#nextLaunch").on("click", ".toggle-header", function(){
+  $("#nextLaunch" ).toggleClass( "hide-details" );
+  $( "#nextLaunch" ).toggleClass( "show-details" );
+});
+// End show additional next launch info
