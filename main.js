d1daca5650939891d8f24807bc2782db07ec69d4
Brennan Butler <bbutler522@gmail.com>
2018-02-06 11:34:16 -0500

    Reorganized for ease of access

--- main.js
+++ main.js
@@ -1,129 +1,16 @@
-// https://api.spacexdata.com/v2/launches
-// https://api.spacexdata.com/v2/launches/upcoming
-
-// Countdown til launch
-// Set the date we're counting down to
-function launchCountdown(launchTime){
-  var countDownDate = new Date(1517941800 * 1000);
-
-  // Update the count down every 1 second
-  var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("launchCountdown").innerHTML = days + "d " + hours + "h "
-      + minutes + "m " + seconds + "s ";
-
-    // If the count down is over, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("launchCountdown").innerHTML = "";
-    }
-    $('#launchCountdown').css('opacity','1')
-  }, 1000);
-}
-// End Countdown Function
-
-
-// Generate next launch
-function nextLaunchGen(json) {
-  var html = "";
-  var val = json[0];
-  var num = 'Next';
-  var launchDate = new Date(val.launch_date_unix * 1000);
-  html += "<div data-launch='" + num + "' class='launch card'>";
-  html += "<div class='card-header toggle-header'>";
-  html += "<p><strong>Flight #:</strong> " + val.flight_number + "</p>";
-  html += "</div>";
-  html += "<div class='card-block'>";
-  html +=
-    "<p class='date'><strong>Launch Date:</strong> " +
-    launchDate.toLocaleDateString() +
-    "</p>";
-  html +=
-    "<p><strong>Rocket Name:</strong> " + val.rocket.rocket_name + "</p>";
-  html +=
-    "<p><strong>Rocket Type:</strong> " + val.rocket.rocket_type + "</p>";
-  var reuse = val.reuse;
-  $.each(reuse, function(key, value) {
-    if (value === true) {
-      key = toTitleCase(key)
-      html += '<p class="text-info"><strong>Reused ' + key + "</strong></p>";
-    }
-  });
-
-  /* Accordion for Details and Payload */
-  html +=
-    '<div class="accordion" id="accordion' +
-    num +
-    '" role="tablist" aria-multiselectable="true">';
-  if (val.details !== null) {
-    html +=
-      '<div class="card border-left-0 border-right-0  border-left-0 border-bottom-0"><div class="card-header" role="tab" id="headingOne' +
-      num +
-      '"><h5 class="mb-0"><a data-toggle="collapse" data-parent="#accordion' +
-      num +
-      '" href="#collapseOne' +
-      num +
-      '" aria-expanded="true" aria-controls="collapseOne' +
-      num +
-      '">Details</a></h5></div><div id="collapseOne' +
-      num +
-      '" class="collapse " role="tabpanel" aria-labelledby="headingOne' +
-      num +
-      '"><div class="card-block">' +
-      val.details +
-      "</div>";
-  }
-
-  html +=
-    '</div></div><div class="card  border-left-0 border-right-0 border-bottom-0"><div class="card-header" role="tab" id="headingThree' +
-    num +
-    '"><h5 class="mb-0"><a class="collapsed" data-toggle="collapse" data-parent="#accordion' +
-    num +
-    '" href="#collapseThree' +
-    num +
-    '" aria-expanded="false" aria-controls="collapseThree' +
-    num +
-    '">Payload</a></h5></div><div id="collapseThree' +
-    num +
-    '" class="collapse" role="tabpanel" aria-labelledby="headingThree' +
-    num +
-    '"><div class="card-block">';
-  // For each payload do
-  var payload = val.rocket.second_stage.payloads;
-  for (i = 0; i < payload.length; i++) {
-    html += "<p><strong>ID:</strong> " + payload[i].payload_id + "</p>";
-    html += "<p><strong>Type:</strong> " + payload[i].payload_type + "</p>";
-
-    html += "<p><strong>Customers:</strong> ";
-    // Get the customers
-    for (j = 0; j < payload[i].customers.length; j++) {
-      html += "<br/>" + payload[i].customers[j];
-    }
-    html += "<hr/>";
-  }
-
-  html += "</div></div></div>";
-  html += "</div>";
-
-  html += "</div>";
-  html += "</div>";
-  html += '<p class="col-12">Stream of launch usually available at <a href="http://www.spacex.com/webcast" target="_blank">http://www.spacex.com/webcast</a></p>'
-  return html;
-
-};
+/* Organization
+- launchGen
+- nextLaunchGen
+- Previous Launches
+- Upcoming Launches
+- Next Launch
+- launchCountdown
+- Sorting
+- Show/Hide Next Launch Detail
+- Make Reusable Parts Readable
+- removeLoad
+- Scroll to Top
+*/
 
 // Function to generate launches from JSON input
 function launchGen(json) {
@@ -165,7 +52,7 @@ function launchGen(json) {
     var reuse = val.reuse;
     $.each(reuse, function(key, value) {
       if (value === true) {
-        key = toTitleCase(key)
+        key = toTitleCase(key);
         html += '<p class="text-info"><strong>Reused ' + key + "</strong></p>";
       }
     });
@@ -238,6 +125,93 @@ function launchGen(json) {
 }
 // End Launch Gen Function
 
+// Generate next launch
+function nextLaunchGen(json) {
+  var html = "";
+  var val = json[0];
+  var num = "Next";
+  var launchDate = new Date(val.launch_date_unix * 1000);
+  html += "<div data-launch='" + num + "' class='launch card'>";
+  html += "<div class='card-header toggle-header'>";
+  html += "<p><strong>Flight #:</strong> " + val.flight_number + "</p>";
+  html += "</div>";
+  html += "<div class='card-block'>";
+  html +=
+    "<p class='date'><strong>Launch Date:</strong> " +
+    launchDate.toLocaleDateString() +
+    "</p>";
+  html += "<p><strong>Rocket Name:</strong> " + val.rocket.rocket_name + "</p>";
+  html += "<p><strong>Rocket Type:</strong> " + val.rocket.rocket_type + "</p>";
+  var reuse = val.reuse;
+  $.each(reuse, function(key, value) {
+    if (value === true) {
+      key = toTitleCase(key);
+      html += '<p class="text-info"><strong>Reused ' + key + "</strong></p>";
+    }
+  });
+
+  /* Accordion for Details and Payload */
+  html +=
+    '<div class="accordion" id="accordion' +
+    num +
+    '" role="tablist" aria-multiselectable="true">';
+  if (val.details !== null) {
+    html +=
+      '<div class="card border-left-0 border-right-0  border-left-0 border-bottom-0"><div class="card-header" role="tab" id="headingOne' +
+      num +
+      '"><h5 class="mb-0"><a data-toggle="collapse" data-parent="#accordion' +
+      num +
+      '" href="#collapseOne' +
+      num +
+      '" aria-expanded="true" aria-controls="collapseOne' +
+      num +
+      '">Details</a></h5></div><div id="collapseOne' +
+      num +
+      '" class="collapse " role="tabpanel" aria-labelledby="headingOne' +
+      num +
+      '"><div class="card-block">' +
+      val.details +
+      "</div>";
+  }
+
+  html +=
+    '</div></div><div class="card  border-left-0 border-right-0 border-bottom-0"><div class="card-header" role="tab" id="headingThree' +
+    num +
+    '"><h5 class="mb-0"><a class="collapsed" data-toggle="collapse" data-parent="#accordion' +
+    num +
+    '" href="#collapseThree' +
+    num +
+    '" aria-expanded="false" aria-controls="collapseThree' +
+    num +
+    '">Payload</a></h5></div><div id="collapseThree' +
+    num +
+    '" class="collapse" role="tabpanel" aria-labelledby="headingThree' +
+    num +
+    '"><div class="card-block">';
+  // For each payload do
+  var payload = val.rocket.second_stage.payloads;
+  for (i = 0; i < payload.length; i++) {
+    html += "<p><strong>ID:</strong> " + payload[i].payload_id + "</p>";
+    html += "<p><strong>Type:</strong> " + payload[i].payload_type + "</p>";
+
+    html += "<p><strong>Customers:</strong> ";
+    // Get the customers
+    for (j = 0; j < payload[i].customers.length; j++) {
+      html += "<br/>" + payload[i].customers[j];
+    }
+    html += "<hr/>";
+  }
+
+  html += "</div></div></div>";
+  html += "</div>";
+
+  html += "</div>";
+  html += "</div>";
+  html +=
+    '<p class="col-12">Stream of launch usually available at <a href="http://www.spacex.com/webcast" target="_blank">http://www.spacex.com/webcast</a></p>';
+  return html;
+}
+
 // Previous Launches
 $.getJSON("https://api.spacexdata.com/v2/launches", function(json) {
   var html = "";
@@ -297,7 +271,6 @@ $.getJSON("https://api.spacexdata.com/v2/launches", function(json) {
     chart.draw(data, google.charts.Bar.convertOptions(options));
   }
 
-
   removeLoad("chartLoad");
 
   $(window).resize(function() {
@@ -336,10 +309,45 @@ $.getJSON("https://api.spacexdata.com/v2/launches/upcoming", function(json) {
 
   // Hide section if there are no listed upcoming launches
   if (json[0] == undefined) {
-    $('#nextLaunch').hide();
+    $("#nextLaunch").hide();
   }
 });
 // End Next Launch
+
+// Countdown til launch
+// Set the date we're counting down to
+function launchCountdown(launchTime) {
+  var countDownDate = new Date(1517941800 * 1000);
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("launchCountdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("launchCountdown").innerHTML = "";
+    }
+    $("#launchCountdown").css("opacity", "1");
+  }, 1000);
+}
+// End Countdown Function
 
 // Sorting Previous Launches
 $("#sortLaunchFirst").on("click", sortLaunchNumFirst);
@@ -367,9 +375,9 @@ function sortLaunchNumLast() {
 // End Sorting Previous Launches
 
 // Show additional next launch info
-$("#nextLaunch").on("click", ".toggle-header", function(){
-  $("#nextLaunch" ).toggleClass( "hide-details" );
-  $( "#nextLaunch" ).toggleClass( "show-details" );
+$("#nextLaunch").on("click", ".toggle-header", function() {
+  $("#nextLaunch").toggleClass("hide-details");
+  $("#nextLaunch").toggleClass("show-details");
 });
 // End show additional next launch info
 
@@ -377,34 +385,40 @@ $("#nextLaunch").on("click", ".toggle-header", function(){
 // Remove underscore and make title case
 function toTitleCase(str) {
   str = str.replace("_", " ");
-    return str.replace(/(?:^|\s)\w/g, function(match) {
-        return match.toUpperCase();
-    });
+  return str.replace(/(?:^|\s)\w/g, function(match) {
+    return match.toUpperCase();
+  });
 }
 // End reused parts readable
 
 // Remove loading icon
-function removeLoad(loadId){
+function removeLoad(loadId) {
   loadId = "#" + loadId;
   $(loadId).remove();
 }
+// End loading icon
 
 // Scroll to top
-$(document).ready(function(){
+$(document).ready(function() {
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $(".scrollToTop").fadeIn();
+    } else {
+      $(".scrollToTop").fadeOut();
+    }
+  });
 
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
+  //Click event to scroll to top
+  $(".scrollToTop").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
 });
 // end scroll to top
+
+// Thanks to https://github.com/r-spacex/SpaceX-API for creating the following APIs
+// https://api.spacexdata.com/v2/launches
+// https://api.spacexdata.com/v2/launches/upcoming
+
+// Project by BrennanButler.com
