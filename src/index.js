c6e4bc8c63a7450a5ceedbdf08614a9f661d60ad
Brennan Butler <bbutler@brunnerworks.com>
2018-02-14 12:39:03 -0500

    Updated smooth scrolling & show above anchor

--- src/index.js
+++ src/index.js
@@ -753,30 +753,22 @@ $(document).ready(function() {
 // end scroll to top
 
 // Add smooth scrolling to nav links
-$(document).ready(function(){
-  $("#navigation").on('click', 'a', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+$(function() {
+  $('a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-200
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
 // End Smooth Scrolling
+
 
 
 // Mobile nav menu functionality
